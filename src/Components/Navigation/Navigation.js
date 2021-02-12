f00c2101865c02d1ea2cc7478ed3e77f3ba3e04b
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2021-02-12 15:13:48 -0500

    moved around navigation folder

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navigation(props) {
+  
+  return (
+    <div>
+      <div className="grid-cols-4">
+
+      </div>
+    </div>
+  )
+};
+
+export default Navigation;
