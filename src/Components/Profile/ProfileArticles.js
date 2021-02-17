f33bf4e31ee492c1b1e13a92e96efe8d7ffdb48a
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2021-02-16 19:43:12 -0500

    i forgot to start a new branch. very minor profile outline work

--- src/Components/Profile/ProfileArticles.js
+++ src/Components/Profile/ProfileArticles.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function ProfileArticles(props) {
-  
+  const [articles, setArticles] = useState([]);
+
+  // we'll set the fetch for the user's saved articles and set the state above
+
   return (
     <div>
-      
+      {/* .map() over the articles here */}
     </div>
   )
 };
